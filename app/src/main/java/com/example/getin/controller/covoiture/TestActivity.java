11d6d2299425b0705c0d87057c7d48f58b165fa5
HayatMh <hayat.ena97@gmail.com>
2019-10-23 16:25:56 +0100

    test activity added

---
+++ app/src/main/java/com/example/getin/controller/covoiture/TestActivity.java
@@ -0,0 +1,16 @@
+package com.example.getin.controller.covoiture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.getin.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
